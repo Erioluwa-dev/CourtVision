63b89c88648c92689e1e195bf8d0faed6855dfab
Erioluwa-dev <erioluwafawehinmi@gmail.com>
2026-06-30 17:03:47 +0000

    tracker

--- hoop.py
+++ hoop.py
@@ -17,3 +17,48 @@ class HoopTracker:
 
         # Whether the hoop was detected
         self.detected = False
+
+    def update(
+        self,
+        hoop_position,
+    ):
+        """
+        Update the hoop location
+        for the current frame.
+        """
+
+        # ----------------------------
+        # Hoop not detected
+        # ----------------------------
+
+        if hoop_position is None:
+
+            self.detected = False
+
+            return
+
+        # ----------------------------
+        # Hoop detected
+        # ----------------------------
+
+        self.detected = True
+
+        # ----------------------------
+        # Save previous position
+        # ----------------------------
+
+        self.last_position = self.current_position
+
+        # ----------------------------
+        # Save current position
+        # ----------------------------
+
+        self.current_position = hoop_position
+
+        # ----------------------------
+        # Store history
+        # ----------------------------
+
+        self.positions.append(
+            hoop_position,
+        )
